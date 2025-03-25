26833be3c24ea3e06851ed970aadd38042c446bc
JoshHyde9 <joshhyde546@gmail.com>
2025-03-25 12:20:42 +1100

    chore: removed unused code from refactor

--- apps/web/hooks/use-conversations.ts
+++ apps/web/hooks/use-conversations.ts
@@ -1,49 +1,16 @@
 import { useEffect } from "react";
 import { useSocket } from "@/providers/ws-provider";
-import { useMutation } from "@tanstack/react-query";
 
 type ConversationsProps = {
   targetId?: string;
 };
 
-type ConversationMessage = {
-  targetId?: string;
-  content?: string;
-  fileUrl?: string;
-  conversationId?: string;
-  messageId?: string;
-  value?: string;
-};
-
 export const useConversations = ({ targetId }: ConversationsProps) => {
-  const {
-    isConnected,
-    joinConversation,
-    sendConversationMessage,
-  } = useSocket();
+  const { isConnected, joinConversation } = useSocket();
 
   useEffect(() => {
     if (targetId && isConnected) {
       joinConversation(targetId);
     }
   }, [targetId, isConnected]);
-
-  const sendMessage = useMutation({
-    mutationFn: async ({
-      content,
-      fileUrl,
-      targetId,
-      conversationId,
-    }: ConversationMessage) => {
-      try {
-        sendConversationMessage({ targetId, content, fileUrl, conversationId });
-
-        return { success: true };
-      } catch (error) {
-        throw error;
-      }
-    },
-  });
-
-  return { sendMessage };
 };
