f92ff22c0a6f5a164423c529a4bd624413a44d31
Josh <joshhyde546@gmail.com>
2025-03-31 15:07:49 +1100

    fix: env

--- packages/auth/src/auth-client.ts
+++ packages/auth/src/auth-client.ts
@@ -1,7 +1,12 @@
 import { createAuthClient } from "better-auth/react";
 
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? process.env.NEXT_PUBLIC_CORS_URL
+    : "http://localhost:5000";
+
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_CORS_URL ?? "http://localhost:5000",
+  baseURL: baseUrl,
 });
 
 export const { signIn, signOut, signUp, useSession } = authClient;
