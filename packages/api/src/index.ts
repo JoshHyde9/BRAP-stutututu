164abd3fa60ccb3468b861920c303003305441a0
Josh <joshhyde546@gmail.com>
2025-04-01 11:12:06 +1100

    fix: elysia url???

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -11,7 +11,7 @@ export type {
 
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? process.env.CORS_URL
+    ? process.env.CORS_URL || process.env.NEXT_PUBLIC_CORS_URL
     : "http://localhost:5000";
 
 export const { api } = treaty<App>(baseUrl!, {
