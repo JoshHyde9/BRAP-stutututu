f804961ee933bae59f894856ce50d71f7919aec6
Josh <joshhyde546@gmail.com>
2025-04-09 17:30:34 +1000

    fix: stop UI from shifting on server channel hover

--- README.md
+++ README.md
@@ -24,7 +24,6 @@ Fkn idk tbh
 - [ ] Fix modal tree closing
 - [ ] Drag and drop file uploads in text channels / conversations
 - [ ] Prefetch everything, app feels very slugish
-- [ ] Fix UI shift when hovering server sidebar items
 - [ ] User settings
   - [ ] Edit their displayName, name and image
 
--- apps/web/components/server/server-channel.tsx
+++ apps/web/components/server/server-channel.tsx
@@ -57,7 +57,7 @@ export const ServerChannel: React.FC<ServerChannelProps> = ({
       <Icon className="size-5 shrink-0 text-zinc-500 dark:text-zinc-400" />
       <p
         className={cn(
-          "line-clamp-1 text-sm font-semibold text-zinc-500 transition group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300",
+          "text-left w-32 truncate line-clamp-1 text-sm font-semibold text-zinc-500 transition group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300",
           params?.channelId === channel.id &&
             "text-primary dark:text-zinc-200 dark:group-hover:text-white",
         )}
